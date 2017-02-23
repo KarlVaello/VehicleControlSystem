e7494fda90c10665200fc537b78102378f16b3e8
KarlVaello <karlvaello@gmail.com>
2017-02-23 02:24:05 +0100

    Graphics update
    Graphics update

--- CCU/Software/CCU/UI/ClusterUI.py
+++ CCU/Software/CCU/UI/ClusterUI.py
@@ -7,6 +7,7 @@ Created on 18 feb. 2017
 from PySide import QtGui, QtCore, QtSvg
 import serial
 from PySide.QtGui import *
+from pip._vendor.html5lib.treewalkers import pprint
 
 
 
@@ -22,15 +23,20 @@ class ClusterUI(QtGui.QWidget):
        
     def initUI(self):
         self.speed = 0
+        self.lastSpeed = 0
+        self.gaugeSpeed = 0
         self.speedFade = 0
         self.menu = 0
         global errorIconOn
         errorIconOn = 1
         
+        
+        print "Load data"
         self.renderer = QtSvg.QSvgRenderer('outiline.svg')        
         self.speedPointerRenderer = QtSvg.QSvgRenderer('speedPointer.svg')   
         self.gpsIconRenderer = QtSvg.QSvgRenderer('gpsIcon.svg')   
         self.musicIconRenderer = QtSvg.QSvgRenderer('musicIcon.svg')   
+        self.errorIconRenderer = QtSvg.QSvgRenderer('errorIcon.svg')   
         
              
         self.clusterDisplayHight = 480
@@ -61,7 +67,7 @@ class ClusterUI(QtGui.QWidget):
         atime = QtCore.QTime.currentTime()
         qtime.setPen(QtGui.QColor(220, 220, 220))
         qtime.setFont(QtGui.QFont('Decorative', 20))
-        qtime.drawText(QtCore.QRect((self.clusterDisplayWidth / 2) -50,15,100,50),QtCore.Qt.AlignCenter,str(atime.hour()) + ":" + str(atime.minute()) + ":" + str(atime.second()))
+        qtime.drawText(QtCore.QRect((self.clusterDisplayWidth / 2) -60,15,120,50),QtCore.Qt.AlignCenter,str(atime.hour()) + ":" + str(atime.minute()) + ":" + str(atime.second()))
         qtime.end()
         
         qspeed = QtGui.QPainter(self)
@@ -72,14 +78,19 @@ class ClusterUI(QtGui.QWidget):
                 
         qp3 = QtGui.QPainter(self)
         qp3.translate((self.clusterDisplayWidth / 2) ,(self.clusterDisplayHight / 2))
-        qp3.rotate(-209 + (int(self.speed)/1.175))
+        qp3.rotate(-209 + (int(self.gaugeSpeed)/1.175))
         self.speedPointerRenderer.render(qp3)
         qp3.end() 
 
         if (errorIconOn == 1):
+            
+            qerrorIconPainter = QtGui.QPainter(self)
+            self.errorIconRenderer.render(qerrorIconPainter,QtCore.QRect(160,100,50,50))
+            qerrorIconPainter.end()
+            
             qp3 = QtGui.QPainter(self)
             errorIconImage = QtGui.QImage("errorIcon.png")
-            qp3.drawImage(QtCore.QRect(160,100,50,50),errorIconImage)
+            qp3.drawImage(QtCore.QRect(260,100,50,50),errorIconImage)
             qp3.end()
 
 
@@ -89,11 +100,15 @@ class ClusterUI(QtGui.QWidget):
         # update GUI current time label  
     def updateSpeed(self,text):
         
-        text2,mn = text.split(';')
+        mn,speedR = text.split(';')
         
-        self.speed = int(mn)
-        self.menu = int(text2)
-        self.update()
+        if(speedR != self.speed or mn != self.menu):
+            if(speedR > self.speed + 2 or speedR < self.speed - 2):
+                self.speed = int(speedR)
+            
+            self.menu = int(mn)
+            self.gaugeSpeed = speedR
+            self.update()
 
     # update GUI current time label
     def updateTime(self):
@@ -118,7 +133,7 @@ class upateThread(QtCore.QThread):
         self.fadeSpeed = 0
     def run(self):
         while True:  
-            self.msleep(0.1)
+            self.msleep(2)
             #QSound.play("beep1.wav")
             #print QSound.isAvailable()
             
